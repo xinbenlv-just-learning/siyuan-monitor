f9d861065e712ea2b5febfabd971c92e5af4d3d7
Zainan Victor Zhou <zzn@zzn.im>
2013-07-16 21:48:34 -0700

    Add first client page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,18 @@
 $('#submitUrl').click(function() {
   alert('submit');
 });
+
+
+var ResultPageCtrl = function($scope) {
+  $scope.hitUrl = 'http://google.com';
+  $scope.rets = [
+    {
+      hitUrl: 'http://google.com',
+      result: true
+    },
+    {
+      hitUrl: 'http://google.com',
+      result: false
+    }
+  ];
+}
